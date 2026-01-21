a8522e0dc4879b795b448b4ed384e742cc691b80
ses9133 <ses9133@gmail.com>
2026-01-21 21:09:47 +0900

    RESTful API 변환

--- src/main/java/org/example/blog/reply/ReplyController.java
+++ src/main/java/org/example/blog/reply/ReplyController.java
@@ -2,38 +2,39 @@ package org.example.blog.reply;
 
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.example.blog._core.constants.SessionConstants;
+import org.example.blog._core.response.ApiResponse;
 import org.example.blog.user.User;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class ReplyController {
     private final ReplyService replyService;
 
-    /**
-     * 댓글 작성 기능 요청
-     * @param saveDTO
-     * @param session
-     * @return
-     */
-    @PostMapping("/reply/save")
+    @PostMapping("/replies")
     public String saveProc(ReplyRequest.SaveDTO saveDTO, HttpSession session) {
-        User sessionUser = (User) session.getAttribute("sessionUser");
-        // 1. 인증 검사() -> 로그인 인터셉터가 인증검사 함
-        // 2. 유효성 검사(형식 검사)
-        // 3. 댓글 작성 요청(서비스단)
-        // 4. 게시글 상세보기 화면 리다이렉트 처리
+        User sessionUser = (User) session.getAttribute(SessionConstants.LOGIN_USER);
         saveDTO.validate();
-        replyService.댓글작성(saveDTO, sessionUser.getId());
-        return "redirect:/board/" + saveDTO.getBoardId();
+        replyService.save(saveDTO, sessionUser.getId());
+        return "redirect:/boards/" + saveDTO.getBoardId();
     }
 
-    @PostMapping("/reply/{replyId}/delete")
-    public String deleteProc(@PathVariable Long replyId, HttpSession session) {
-        User sessionUser = (User) session.getAttribute("sessionUser");
-        Long boardId = replyService.댓글삭제(replyId, sessionUser.getId());
-        return "redirect:/board/" + boardId;
+    @GetMapping("/board/{boardId}/replies")
+    public List<ReplyResponse.ListDTO> replyList(@PathVariable Long boardId, HttpSession session) {
+        User sessionUser = (User) session.getAttribute(SessionConstants.LOGIN_USER);
+        return replyService.replyList(boardId, sessionUser.getId());
+    }
+
+    @DeleteMapping("/api/v1/replies/{replyId}")
+    @ResponseBody
+    public ResponseEntity<ApiResponse<Void>> delete(@PathVariable Long replyId, HttpSession session) {
+        User sessionUser = (User) session.getAttribute(SessionConstants.LOGIN_USER);
+        replyService.delete(replyId, sessionUser.getId());
+        return ResponseEntity.ok(ApiResponse.ok("댓글 삭제에 성공했습니다."));
     }
 }
