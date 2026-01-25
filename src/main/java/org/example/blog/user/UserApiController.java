e5c1cd0cbd7b3142e94bbbd190f026e8d46dff16
ses9133 <ses9133@gmail.com>
2026-01-25 15:03:52 +0900

    컨트롤러 분리중

--- src/main/java/org/example/blog/user/UserApiController.java
+++ src/main/java/org/example/blog/user/UserApiController.java
@@ -12,39 +12,9 @@ import java.util.Map;
 @RestController
 @RequiredArgsConstructor
 public class UserApiController {
-    private final MailService mailService;
     private final UserService userService;
 
-    @PostMapping("/api/email/send")
-    public ResponseEntity<?> 인증번호발송(@RequestBody UserRequest.EmailCheckDTO reqDTO) {
-        // 1. 유효성 검사
-        reqDTO.validate();
-
-        // 2. 서비스단에서 구글 메일 서버로 이메일 전송 처리
-        mailService.인증번호발송(reqDTO.getEmail());
-
-        return ResponseEntity.ok().body(Map.of("message", "인증번호가 발송되었습니다."));
-    }
-
-    @PostMapping("/api/email/verify")
-    public ResponseEntity<?> 인증번호확인(@RequestBody UserRequest.EmailCheckDTO reqDTO) {
-        reqDTO.validate();
-
-        if(reqDTO.getCode().isEmpty() || reqDTO.getCode().trim().isEmpty()) {
-            return ResponseEntity.badRequest().body(Map.of("message", "인증번호를 입력해주세요"));
-        }
-
-        // 서비스단에서 인증번호 확인
-        boolean isVerified = mailService.인증번호확인(reqDTO.getEmail(), reqDTO.getCode());
-
-        // 결과값에 따라 분기처리
-        if(isVerified) {
-            return ResponseEntity.ok().body(Map.of("message", "인증되었습니다."));
-        } else {
-            return ResponseEntity.badRequest().body(Map.of("message", "인증 실패하였습니다."));
-        }
-    }
-
+    // TODO - 포인트 관련 리팩토링 필요
     // api/point/charge
     @PostMapping("/api/point/charge")
     public ResponseEntity<?> chargePoint(@RequestBody UserRequest.PointChargeDTO reqDTO, HttpSession session) {
