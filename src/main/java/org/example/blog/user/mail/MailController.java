b9d35716780831b27307fe51c3bf3d6c5e75d5e2
ses9133 <ses9133@gmail.com>
2026-01-25 15:01:58 +0900

    Mail 도메인 폴더 분리

--- src/main/java/org/example/blog/user/mail/MailController.java
+++ src/main/java/org/example/blog/user/mail/MailController.java
@@ -1,4 +1,43 @@
 package org.example.blog.user.mail;
 
+import lombok.RequiredArgsConstructor;
+import org.example.blog._core.response.ApiResponse;
+import org.example.blog.user.UserRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/email")
+@RequiredArgsConstructor
 public class MailController {
+    private final MailService mailService;
+
+    @PostMapping("/send")
+    public ResponseEntity<ApiResponse<Void>> sendVerificationEmail(@RequestBody UserRequest.EmailCheckDTO reqDTO) {
+        reqDTO.validate();
+
+        mailService.sendVerificationEmail(reqDTO.getEmail());
+
+        return ResponseEntity.ok().body(ApiResponse.ok("인증번호 발송에 성공했습니다."));
+    }
+
+    @PostMapping("/verify")
+    public ResponseEntity<ApiResponse<Void>> verifyEmailCode(@RequestBody UserRequest.EmailCheckDTO reqDTO) {
+        reqDTO.validate();
+
+        if(reqDTO.getCode().isEmpty() || reqDTO.getCode().trim().isEmpty()) {
+            return ResponseEntity.badRequest().body(ApiResponse.fail("유효한 인증코드가 아닙니다."));
+        }
+
+        boolean isVerified = mailService.verifyEmailCode(reqDTO.getEmail(), reqDTO.getCode());
+
+        if(isVerified) {
+            return ResponseEntity.ok().body(ApiResponse.ok("이메일 인증에 성공했습니다."));
+        } else {
+            return ResponseEntity.badRequest().body(ApiResponse.fail("이메일 인증에 실패했습니다."));
+        }
+    }
 }
--- src/main/java/org/example/blog/user/mail/MailService.java
+++ src/main/java/org/example/blog/user/mail/MailService.java
@@ -1,8 +1,9 @@
-package org.example.blog.user;
+package org.example.blog.user.mail;
 
 import jakarta.mail.internet.MimeMessage;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.example.blog._core.errors.exception.Exception500;
 import org.example.blog._core.utils.MailUtils;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -14,49 +15,40 @@ public class MailService {
     private final JavaMailSender javaMailSender;
     private final HttpSession session;
 
-    public void 인증번호발송(String email) {
-        // 1. email -> 인증번호 (123456) -> 임시로 세션 메모리 저장 -> 메일 발송 요청
+    public void sendVerificationEmail(String email) {
+        // 1. 랜덤코드 생성
         String code = MailUtils.generateRandomCode();
 
         // 2. 이메일 전송 내용 설정
-        // MimeMessage(텍스트 뿐만 아니라 HTML, 첨부파일 포함할 수 있는 표준 포맷) / SimpleMailMessage(순수 텍스만 보낼떄 사용)
         MimeMessage message = javaMailSender.createMimeMessage();
 
-        // 3. 구글 메일 서버로 전송 - 우리 서버가 아니고 외부 서버로 통신 요청
-        // 외부 통신하는 코드일 때도 기본적으로 try ~ catch 사용 권장
+        // 3. 구글 메일 서버로 전송(외부 서버로 통신 요청)
         try {
             // 3.1 도우미 객체를 사용
-            // 매개변수 내용: (메시지 내용, 멀티파트 허용 여부, 인코딩 설정)
             MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
 
-            helper.setTo(email); // email: 받는 사람 이메일 주소
-            helper.setSubject("[MyBlog] 회원가입 이메일 전송"); // 메일의 제목 설정
-            helper.setText("<h3>인증번호는 [" + code + "] 입니다. </h3>", true); // 메일 내용 설정\
+            helper.setTo(email); // 수신 이메일 설정
+            helper.setSubject("[MyBlog] 회원가입 이메일 전송"); // 메일 제목 설정
+            helper.setText("<h3>인증번호는 [" + code + "] 입니다. </h3>", true); // 메일 내용 설정
 
             javaMailSender.send(message);
 
             // 4. 세션에 임시 코드 저장
-            // code_ses9133@naver.com: 123456
-            // 동시에 접속자가 많아도 이메일 주소로 누구의 인증 번호인지 구별할 수 있음
             session.setAttribute("code_" + email, code);
-
+            // 왜 DB가 아닌 세션에 저장하는 이유 ?
+            // 로그인 전 단계인 회원가입 과정에서 임시 데이터를 가볍게 관리하기 위해 서버 세션을 활용
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            throw new Exception500("이메일 인증번호 전송 중 오류가 발생했습니다.");
         }
-
     }
 
-    public boolean 인증번호확인(String email, String code) {
-        // 1. 세션에 저장된 코드 가져오기
+    public boolean verifyEmailCode(String email, String code) {
         String savedCode = (String) session.getAttribute("code_" + email);
 
-        // 2. 세션에서 가져온 코드 값과 사용자가 입력한 인증번호가 같은지 확인
-        if(savedCode != null && savedCode.equals(code)) {
-            // ** 확인하고 나서 세션 메모리에서 제거해주어야함
+        if (savedCode != null && savedCode.equals(code)) {
             session.removeAttribute("code_" + email);
             return true;
         }
-
         return false;
     }
 }
