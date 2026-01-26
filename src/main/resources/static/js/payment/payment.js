5c19c4865d14b3091b64c17bba9f1fecbbb4ece9
ses9133 <ses9133@gmail.com>
2026-01-26 21:20:07 +0900

    포트원 API V1 -> V2 변경

---
+++ src/main/resources/static/js/payment/payment.js
@@ -0,0 +1,90 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    let selectedAmount = 0;
+
+    const $btns = document.querySelectorAll('.point-btn');
+    const $displayAmount = document.getElementById('selectedAmount');
+    const $chargeBtn = document.getElementById('chargeBtn');
+
+    function updateAmount(amount) {
+        selectedAmount = amount;
+        $displayAmount.textContent = amount.toLocaleString();
+
+        $chargeBtn.disabled = amount <= 0;
+    }
+
+    $btns.forEach(btn => {
+        btn.addEventListener('click', function () {
+            $btns.forEach(b => b.classList.remove('active'));
+            this.classList.add('active');
+
+            const amount = parseInt(this.getAttribute('data-amount'));
+            updateAmount(amount);
+        });
+    });
+
+    $chargeBtn.addEventListener('click', async function () {
+        try {
+            // 1. 결제 번호 생성
+            const res = await fetch("/api/v1/payment/prepare", {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({
+                    amount: selectedAmount
+                })
+            });
+
+            const data = await res.json();
+            const paymentId = data.data.paymentId;
+
+            // 2. 결제 요청 (V2 방식)
+            const response = await PortOne.requestPayment({
+                storeId: "store-d8111b19-87ed-4114-88b0-5372f91a9a40",
+                channelKey: 'channel-key-bf8b0eec-e270-4476-808b-5392d4ca19ce',
+                paymentId: paymentId,
+                orderName: "포인트 충전",
+                totalAmount: Number(selectedAmount),
+                currency: "CURRENCY_KRW",
+                payMethod: "EASY_PAY"
+            });
+
+            // 3. 결제 결과 처리
+            if (response.code != null) {
+                return alert("결제 실패: " + response.message);
+            }
+
+            // 4. 백엔드 검증 API 호출
+            verifyPaymentAndCharge(response.paymentId);
+        } catch (error) {
+            alert("결제 요청 실패");
+        }
+    });
+
+
+    function verifyPaymentAndCharge(payment_id) {
+        fetch('/api/v1/payment/verify', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                paymentId: payment_id
+            })
+        }).then(res => {
+
+            if (!res.ok) {
+                return res.json().then((e) => {
+                    throw new Error(e.message);
+                });
+            }
+            return res.json();
+        })
+            .then(data => {
+                alert(`${data.data.amount} 포인트 충전이 완료되었습니다.`);
+                location.href = '/me';
+            })
+            .catch(() => {
+                alert('결제 검증에 실패했습니다\n관리자에게 문의하세요');
+            });
+    }
+});
