ef5bb06cd72069325e9f763ed14272d6b529c5dc
ses9133 <ses9133@gmail.com>
2026-01-26 21:12:13 +0900

    커스텀 예외 변경

--- src/main/java/org/example/blog/_core/utils/DateUtil.java
+++ src/main/java/org/example/blog/_core/utils/DateUtil.java
@@ -1,17 +1,18 @@
 package org.example.blog._core.utils;
 
+import org.example.blog._core.errors.exception.Exception400;
+
 import java.sql.Timestamp;
 import java.time.format.DateTimeFormatter;
 
-public class MyDateUtil {
+public class DateUtil {
 
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     public static String format(Timestamp timestamp) {
         if (timestamp == null) {
-            throw new RuntimeException("timestamp는 비어있을 수 없습니다.");
+            throw new Exception400("timestamp는 비어있을 수 없습니다.");
         }
-
          return timestamp.toLocalDateTime().format(FORMATTER);
     }
 }
