e87cbea74a052296fac9b0ab5847bfd863b5359e
ses9133 <ses9133@gmail.com>
2026-01-26 21:11:18 +0900

    리소스 핸들러 변경 및 로그인 인터셉터 path 추가

--- src/main/java/org/example/blog/_core/config/WebMvcConfig.java
+++ src/main/java/org/example/blog/_core/config/WebMvcConfig.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.example.blog._core.interceptor.AdminInterceptor;
 import org.example.blog._core.interceptor.LoginInterceptor;
 import org.example.blog._core.interceptor.SessionInterceptor;
+import org.example.blog._core.utils.FileUtil;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -20,22 +22,33 @@ public class WebMvcConfig implements WebMvcConfigurer {
     private final SessionInterceptor sessionInterceptor;
     private final AdminInterceptor adminInterceptor;
 
+    private final FileUtil fileUtil;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(sessionInterceptor)
                         .addPathPatterns("/**");
 
         registry.addInterceptor(loginInterceptor)
-                .addPathPatterns("/board/**", "/user/**", "/reply/**", "/admin/**")
+                .addPathPatterns(
+                        "/boards/**",
+                        "/api/v1/boards/**",
+                        "/api/v1/me/**",
+                        "/api/v1/payment/**",
+                        "/user/**",
+                        "/replies/**",
+                        "/logout",
+                        "/admin/**",
+                        "/me/**"
+                )
                 .excludePathPatterns(
                         "/login/**",
                         "/join",
-                        "/logout",
                         "/user/kakao",
                         "/user/naver",
-                        "/board/list",
+                        "/boards",
                         "/",
-                        "/board/{id:\\d+}",
+                        "/boards/{id:\\d+}",
                         "/css/**",
                         "/js/**",
                         "/images/**",
@@ -50,7 +63,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/**")
-                .addResourceLocations("file:///C:/uploads/");
+                .addResourceLocations("file:///" + fileUtil.getUploadDir() + "/");
     }
 
     @Bean
---
+++ src/main/java/org/example/blog/payment/PaymentStatus.java
@@ -0,0 +1,4 @@
+package org.example.blog.payment;
+
+public enum PaymentStatus {
+}
