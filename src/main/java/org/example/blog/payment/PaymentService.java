2be03eaf31eb3e2a4f7b76251a4801366ce913ea
ses9133 <ses9133@gmail.com>
2026-01-27 16:57:50 +0900

    결제내역 조회 분기 처리 수정

--- src/main/java/org/example/blog/payment/PaymentService.java
+++ src/main/java/org/example/blog/payment/PaymentService.java
@@ -4,8 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.example.blog._core.errors.exception.Exception400;
 import org.example.blog._core.errors.exception.Exception404;
 import org.example.blog._core.errors.exception.Exception500;
-import org.example.blog.refund.RefundRequest;
-import org.example.blog.refund.RefundRequestRepository;
+import org.example.blog.refund.Refund;
+import org.example.blog.refund.RefundRepository;
 import org.example.blog.user.User;
 import org.example.blog.user.UserRepository;
 import org.springframework.beans.factory.annotation.Value;
@@ -23,7 +23,7 @@ public class PaymentService {
 
     private final PaymentRepository paymentRepository;
     private final UserRepository userRepository;
-    private final RefundRequestRepository refundRequestRepository;
+    private final RefundRepository refundRepository;
 
     @Value("${portone.imp-secret}")
     private String impSecret;
@@ -48,7 +48,6 @@ public class PaymentService {
 
     @Transactional
     public PaymentResponse.VerifyDTO verifyPaymentAndCharge(Long userId, String paymentId) {
-
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new Exception404("사용자를 찾을 수 없습니다"));
 
@@ -60,9 +59,6 @@ public class PaymentService {
         });
 
         PaymentResponse.PortOneV2Response paymentData = getPortOnePayment(paymentId);
-
-        // 결제 금액 위변조 방지
-
 
         user.chargePoint(paymentData.getAmount().getTotal());
 
@@ -84,6 +80,9 @@ public class PaymentService {
         try {
             RestTemplate restTemplate = new RestTemplate();
 
+            System.out.println("=== 포트원 결제 조회 시작 ===");
+            System.out.println("요청 PaymentID: " + paymentId);
+
             HttpHeaders headers = new HttpHeaders();
             headers.set("Authorization", "PortOne " + impSecret);
             headers.setContentType(MediaType.APPLICATION_JSON);
@@ -96,13 +95,14 @@ public class PaymentService {
                     request,
                     PaymentResponse.PortOneV2Response.class);
 
+            System.out.println("포트원 응답 상태 코드: " + response.getStatusCode());
+
             PaymentResponse.PortOneV2Response data = response.getBody();
 
             if (data == null) {
                 throw new Exception400("결제 정보를 찾을 수 없습니다.");
             }
 
-            // ** 데이터 무결성 검증 **
             // 결제 상태 확인
             if (!"PAID".equals(data.getStatus())) {
                 throw new Exception400("결제가 완료되지 않았습니다.");
@@ -119,40 +119,21 @@ public class PaymentService {
         }
     }
 
-    // TODO - 환불 요청 관련 리팩토링 필요
     public List<PaymentResponse.ListDTO> paymentList(Long userId) {
         List<Payment> paymentList = paymentRepository.findByUserId(userId);
 
         return paymentList.stream()
                 .map(payment -> {
-                    // 환불 요청 조회
-                    // 결제 PK 값으로 환불 테이블에 이력이 있는지 없는지 조회
-                    Optional<RefundRequest> refundRequestOpt = refundRequestRepository.findByPaymentId(payment.getId());
+                    Optional<Refund> refundOpt = refundRepository.findByPaymentId(payment.getId());
 
-                    // 환불 요청이 있는 경우 상태 확인
-                    // 요청이 있으면 화면에 '환불 요청' 버튼 보이면 안됨
-                    boolean hasRefundRequest = refundRequestOpt.isPresent();
                     boolean isRefundable = false;
 
-                    if("paid".equals(payment.getPaymentStatus().toString())) {
-                        // 결제 완료인 상태
-                        if(!hasRefundRequest) {
-                            // 환불 요청이 없는 상태 -> 환불 가능한 상태임
+                    if(payment.isPaid()) {
+                        if(refundOpt.isPresent() && refundOpt.get().isPending()) {
                             isRefundable = true;
-                        } else {
-                            // 환불 요청 대기 상태 --> 원래 isRefundable = false 임 (즉 화면ㅇ ㅔ버튼 안보임)
-                            RefundRequest refundRequest = refundRequestOpt.get();
-                            //관리자가 환불 거절했지만 다시 요청할수있게 한다면
-                            if(refundRequest.isRejected()) {
-                                isRefundable = true;
-                            } else {
-                                // 대기중 / 환불 완료 상태
-                                isRefundable = false;
-                            }
+                        } else if(refundOpt.isEmpty()) {
+                            isRefundable = true;
                         }
-                    } else {
-                        // 환불 완료 상태 (돈 내어줌)
-                        isRefundable = false;
                     }
                     return new PaymentResponse.ListDTO(payment, isRefundable);
                 }).toList();
