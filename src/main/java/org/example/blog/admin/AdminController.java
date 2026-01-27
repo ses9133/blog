4ac897794d48a429e8a71ceec5f48356f5b8ba00
ses9133 <ses9133@gmail.com>
2026-01-27 16:56:45 +0900

    URL 수정

--- src/main/java/org/example/blog/admin/AdminController.java
+++ src/main/java/org/example/blog/admin/AdminController.java
@@ -2,56 +2,50 @@ package org.example.blog.admin;
 
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.example.blog._core.constants.SessionConstants;
 import org.example.blog._core.errors.exception.Exception400;
 import org.example.blog.refund.RefundResponse;
 import org.example.blog.refund.RefundService;
 import org.example.blog.user.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/admin")
 public class AdminController {
     private final RefundService refundService;
 
-    // http://localhost:8080/admin/dashboard
-    @GetMapping("/admin/dashboard")
+    @GetMapping("/dashboard")
     public String dashboard(HttpSession session, Model model) {
-        User sessionUser = (User) session.getAttribute("sessionUser");
+        User sessionUser = (User) session.getAttribute(SessionConstants.LOGIN_USER);
         model.addAttribute("user", sessionUser);
         return "admin/dashboard";
     }
 
-    // 관리자 환불 요청 관리 목록 페이지
-    @GetMapping("/admin/refund/list")
-    public String refundManagement(Model model) {
-        List<RefundResponse.AdminListDTO> refundList = refundService.관리자환불요청목록조회();
+    @GetMapping("/refunds")
+    public String getAllRefundsForAdmin(Model model) {
+        List<RefundResponse.AdminListDTO> refundList = refundService.getAllRefundsForAdmin();
         model.addAttribute("refundList", refundList);
         return "admin/admin-refund-list";
     }
 
-    // `/admin/refund/${id}/reject`
-    @PostMapping("/admin/refund/{id}/reject")
-    // DTO 안만들고 RequestParam 으로 바로 추출
+    @PostMapping("/refunds/{id}/reject")
     public String rejectRefund(@PathVariable Long id, @RequestParam(name = "rejectReason") String rejectReason) {
         if(rejectReason == null || rejectReason.trim().isEmpty()) {
             throw new Exception400("거절 사유를 입력해주세요");
         }
-        refundService.환불거절(id, rejectReason);
-        return "redirect:/admin/refund/list";
+        refundService.rejectRefund(id, rejectReason);
+        return "redirect:/admin/refunds";
     }
 
-    // /admin/refund/${id}/approve
-    @PostMapping("/admin/refund/{id}/approve")
+    @PostMapping("/refunds/{id}/approve")
     public String approveRefund(@PathVariable Long id) {
-        refundService.환불승인(id);
-        return "redirect:/admin/refund/list";
+        refundService.approveRefund(id);
+        return "redirect:/admin/refunds";
     }
 }
 
