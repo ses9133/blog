df56501c296533444edfb446263722b284ecee4f
ses9133 <ses9133@gmail.com>
2026-01-27 16:58:16 +0900

    URL 수정

--- src/main/java/org/example/blog/refund/RefundController.java
+++ src/main/java/org/example/blog/refund/RefundController.java
@@ -2,6 +2,7 @@ package org.example.blog.refund;
 
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.example.blog._core.constants.SessionConstants;
 import org.example.blog._core.errors.exception.Exception401;
 import org.example.blog.payment.Payment;
 import org.example.blog.payment.PaymentResponse;
@@ -14,43 +15,32 @@ import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/refunds")
 public class RefundController {
     private final RefundService refundService;
 
-    @GetMapping("/refund/request/{paymentId}")
+    @GetMapping("/{paymentId}")
     public String refundRequestForm(@PathVariable Long paymentId, Model model, HttpSession session) {
-        // 인증 인터셉터 아직 적용전
-        // TODO 인터셉터 추가
-        User sessionUser = (User) session.getAttribute("sessionUser");
-
-        // 서비스에 paymentId 관련 정보를 요청
-        Payment payment = refundService.환불요청폼화면검증(paymentId, sessionUser.getId());
+        User sessionUser = (User) session.getAttribute(SessionConstants.LOGIN_USER);
+        Payment payment = refundService.refundRequestForm(paymentId, sessionUser.getId());
         PaymentResponse.ListDTO paymentDTO = new PaymentResponse.ListDTO(payment);
         model.addAttribute("payment", paymentDTO);
-
         return "refund/request-form";
     }
 
-    @PostMapping("/refund/request")
-//    @ResponseBody
-    public String refundRequest(RefundRequestDTO.RequestDTO reqDTO, HttpSession session) {
-        User sessionUser = (User) session.getAttribute("sessionUser");
-        if(sessionUser == null) {
-            throw new Exception401("로그인이 필요합니다.");
-        }
+    @PostMapping
+    public String refundRequest(RefundRequest.DTO reqDTO, HttpSession session) {
+        User sessionUser = (User) session.getAttribute(SessionConstants.LOGIN_USER);
         reqDTO.validate();
-        refundService.환불요청(sessionUser.getId(), reqDTO);
-        return "redirect:/refund/list";
+        refundService.refundRequest(sessionUser.getId(), reqDTO);
+        return "redirect:/refunds";
     }
 
     // 내 환불 요청 내역 조회
-    @GetMapping("/refund/list")
+    @GetMapping
     public String refundList(Model model, HttpSession session) {
-        User sessionUser = (User) session.getAttribute("sessionUser");
-        if(sessionUser == null) {
-            throw new Exception401("로그인이 필요합니다.");
-        }
-        List<RefundResponse.ListDTO> refundList = refundService.환불요청목록조회(sessionUser.getId());
+        User sessionUser = (User) session.getAttribute(SessionConstants.LOGIN_USER);
+        List<RefundResponse.ListDTO> refundList = refundService.refundList(sessionUser.getId());
         model.addAttribute("refundList", refundList);
         return "refund/list";
     }
