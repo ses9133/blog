7d04781e913e7a3041acda14b35f93b125f4546e
ses9133 <ses9133@gmail.com>
2026-01-28 14:58:42 +0900

    결제 금액 위변조 방지 코드 추가

--- src/main/java/org/example/blog/payment/PaymentService.java
+++ src/main/java/org/example/blog/payment/PaymentService.java
@@ -8,6 +8,7 @@ import org.example.blog.refund.Refund;
 import org.example.blog.refund.RefundRepository;
 import org.example.blog.user.User;
 import org.example.blog.user.UserRepository;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
@@ -30,16 +31,24 @@ public class PaymentService {
 
     @Transactional
     public PaymentResponse.PrepareDTO preparePayment(Long userId, Integer amount) {
-        if(!userRepository.existsById(userId)) {
-            throw new Exception400("사용자를 찾을 수 없습니다.");
-        }
+        User userEntity = userRepository.findById(userId)
+                .orElseThrow(() -> new Exception400("사용자를 찾을 수 없습니다."));
 
         String paymentId = generatePaymentId();
         while (paymentRepository.existsByPaymentId(paymentId)) {
             paymentId = generatePaymentId();
         }
 
-        return new PaymentResponse.PrepareDTO(paymentId, amount);
+        Payment payment = Payment.builder()
+                .paymentId(paymentId)
+                .user(userEntity)
+                .amount(amount)
+                .paymentStatus(PaymentStatus.READY)
+                .build();
+
+        paymentRepository.save(payment);
+
+        return new PaymentResponse.PrepareDTO(payment.getPaymentId(), amount);
     }
 
     private String generatePaymentId() {
@@ -48,30 +57,40 @@ public class PaymentService {
 
     @Transactional
     public PaymentResponse.VerifyDTO verifyPaymentAndCharge(Long userId, String paymentId) {
-        User user = userRepository.findById(userId)
+        User userEntity = userRepository.findById(userId)
                 .orElseThrow(() -> new Exception404("사용자를 찾을 수 없습니다"));
 
+        Optional<Payment> paymentOpt = paymentRepository.findByPaymentId(paymentId);
+
+        // 유효한 결제 요청인지 확인
+        if(paymentOpt.isEmpty()) {
+            throw new Exception400("결제 요청을 찾을 수 없습니다.");
+        }
+
+        Payment payment = paymentOpt.get();
+
         // 중복 결제 방지
-        paymentRepository.findByPaymentId(paymentId).ifPresent(payment -> {
-            if(payment.isPaid()) {
-                throw new Exception400("이미 결제된 내역입니다.");
-            }
-        });
+        if(payment.isPaid()) {
+            throw new Exception400("이미 결제된 내역입니다.");
+        }
+
+        // 결제 상태 확인
+        if(payment.isCancelled()) {
+            throw new Exception400("취소된 결제내역은 재결제 불가능합니다.");
+        }
 
         PaymentResponse.PortOneV2Response paymentData = getPortOnePayment(paymentId);
+        Integer responseAmount = paymentData.getAmount().getTotal();
 
-        user.chargePoint(paymentData.getAmount().getTotal());
+        // 결제 금액 위변조 방지
+        if(!responseAmount.equals(payment.getAmount())) {
+            throw new Exception400("결제 요청 금액이 불일치합니다.");
+        }
 
-        Payment payment = Payment.builder()
-                .paymentId(paymentId)
-                .user(user)
-                .amount(paymentData.getAmount().getTotal())
-                .paymentStatus(PaymentStatus.PAID)
-                .build();
+        userEntity.chargePoint(paymentData.getAmount().getTotal());
+        payment.updateStatus(PaymentStatus.PAID);
 
-        paymentRepository.save(payment);
-
-        return new PaymentResponse.VerifyDTO(paymentData.getAmount().getTotal(), user.getPoint());
+        return new PaymentResponse.VerifyDTO(paymentData.getAmount().getTotal(), userEntity.getPoint());
     }
 
     // 포트원 결제 조회
@@ -79,9 +98,6 @@ public class PaymentService {
 
         try {
             RestTemplate restTemplate = new RestTemplate();
-
-            System.out.println("=== 포트원 결제 조회 시작 ===");
-            System.out.println("요청 PaymentID: " + paymentId);
 
             HttpHeaders headers = new HttpHeaders();
             headers.set("Authorization", "PortOne " + impSecret);
@@ -94,8 +110,6 @@ public class PaymentService {
                     HttpMethod.GET,
                     request,
                     PaymentResponse.PortOneV2Response.class);
-
-            System.out.println("포트원 응답 상태 코드: " + response.getStatusCode());
 
             PaymentResponse.PortOneV2Response data = response.getBody();
 
