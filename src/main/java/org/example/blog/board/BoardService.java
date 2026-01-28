4eb6e871b2e5a19bcf88e0ee9c959ea245be9f88
ses9133 <ses9133@gmail.com>
2026-01-28 14:37:08 +0900

    주석 정리

--- src/main/java/org/example/blog/board/BoardService.java
+++ src/main/java/org/example/blog/board/BoardService.java
@@ -13,7 +13,6 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
